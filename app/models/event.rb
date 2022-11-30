f0f07a79ec7d9e1c64187fcf64432caa49aabfef
Redouane Elmahi <redouaneelmahi@gmail.com>
2022-11-30 14:54:50 +0100

    add some validates

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :colocation
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- app/models/user_colocation.rb
+++ app/models/user_colocation.rb
@@ -1,4 +1,7 @@
 class UserColocation < ApplicationRecord
-  belongs_to :user_id
-  belongs_to :colocation_id
+  # belongs_to :user   A VOIR
+  # belongs_to :colocation_id
+  has_many :users
+  # has_many :colocations
+  belongs_to :colocation
 end
