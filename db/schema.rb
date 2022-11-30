f9e45d912e3390cedb053d025c44eac64261852d
Redouane Elmahi <redouaneelmahi@gmail.com>
2022-11-30 12:49:29 +0100

    created event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_112953) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_114007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_112953) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string "content"
+    t.bigint "colocation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["colocation_id"], name: "index_events_on_colocation_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -72,6 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_112953) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "colocations"
   add_foreign_key "items", "lists"
   add_foreign_key "lists", "colocations"
   add_foreign_key "notes", "colocations"
