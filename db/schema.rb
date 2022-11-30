b6e8f21b045aa553a3512556b133cbeb8f05b486
Redouane Elmahi <redouaneelmahi@gmail.com>
2022-11-30 12:29:07 +0100

    created item model refer to list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_111747) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_112155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_111747) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.boolean "completed", default: false
+    t.boolean "urgent", default: false
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_items_on_list_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -52,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_111747) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "lists"
   add_foreign_key "lists", "colocations"
   add_foreign_key "user_colocations", "colocations"
   add_foreign_key "user_colocations", "users"
