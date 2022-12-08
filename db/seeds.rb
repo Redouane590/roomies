49f19090f5537f0787868ba50b97aef17f827052
Roman Bajine <bajine.roman@gmail.com>
2022-12-08 17:22:02 +0100

    seed updated 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Colocation.destroy_all
 User.destroy_all
 puts "create friends colo"
 friends = Colocation.create!(name: "Friends")
-Chatroom.create!(colocation_id: friends.id)
+chatroom = Chatroom.create!(colocation_id: friends.id)
 puts "users creation.."
 file_joey = URI.open("https://i.pinimg.com/originals/32/cb/60/32cb600629bfdad9cbe5f138a67dc7d3.jpg")
 joey = User.create!(email: "joey@mail.fr", nickname: "Joey", password: "azerty", phone_number: "0628013779")
@@ -35,13 +35,16 @@ file_monica = URI.open("https://static.wikia.nocookie.net/friends/images/7/75/Mo
 monica = User.create!(email: "monica@mail.fr", nickname: "Monica", password: "azerty", phone_number: "0738013791")
 monica.photo.attach(io: file_monica, filename: "Joey.jpg", content_type: "image/jpg")
 monica.save
+file_phoebe = URI.open("https://static.wikia.nocookie.net/friends/images/7/79/Phoebe.jpg/revision/latest/scale-to-width-down/180?cb=20120326154329&path-prefix=fr")
+phoebe = User.create!(email: "phoebe@mail.fr", nickname: "Phoebe", password: "azerty", phone_number: "0738013744")
+phoebe.photo.attach(io: file_phoebe, filename: "Joey.jpg", content_type: "image/jpg")
+phoebe.save
 puts "usercoloc creation.."
 UserColocation.create!(colocation_id: friends.id, user_id: joey.id)
 UserColocation.create!(colocation_id: friends.id, user_id: rachel.id)
 UserColocation.create!(colocation_id: friends.id, user_id: ross.id)
 UserColocation.create!(colocation_id: friends.id, user_id: chandler.id)
 UserColocation.create!(colocation_id: friends.id, user_id: monica.id)
-puts "all done"
 puts "create notes..."
 Note.create(title: "Mot de passe wifi", content: "Asx@l597#ffez", colocation_id: friends.id)
 Note.create(title: "Numero du proprio", content: "0783407855", colocation_id: friends.id)
@@ -51,3 +54,17 @@ Note.create(title: "Soirée de noel", content: "Oubliez pas les preparatifs de n
 Note.create(title: "Planning de menage", content: "N'oubliez de faire le ménage quand c'est votre tour (voir planning)", colocation_id: friends.id)
 Note.create(title: "Bug de la TV", content: "Quand la télé ne veux pas s'allumer mettez lui un coup de boule et rallumez la !", colocation_id: friends.id)
 Note.create(title: "Anniv de rachel", content: "Le 10 décembre !!", colocation_id: friends.id)
+puts "create messages..."
+Message.create(chatroom_id: chatroom.id, user_id: joey.id, content: "Bonjour la coloc' !!!")
+Message.create(chatroom_id: chatroom.id, user_id: ross.id, content: "Salut Jo !")
+Message.create(chatroom_id: chatroom.id, user_id: monica.id, content: "Bonjour les gars !")
+Message.create(chatroom_id: chatroom.id, user_id: joey.id, content: "Comment allez-vous ?")
+Message.create(chatroom_id: chatroom.id, user_id: ross.id, content: "Tout roule et toi ?")
+Message.create(chatroom_id: chatroom.id, user_id: joey.id, content: "Rachel n'est pas là ?")
+Message.create(chatroom_id: chatroom.id, user_id: rachel.id, content: "Si si je suis là ! Tout le monde va bien ?")
+Message.create(chatroom_id: chatroom.id, user_id: monica.id, content: "Oui, hâte de finir le travail !")
+Message.create(chatroom_id: chatroom.id, user_id: joey.id, content: "P'tite bière ?")
+Message.create(chatroom_id: chatroom.id, user_id: ross.id, content: "Je suis partant !")
+Message.create(chatroom_id: chatroom.id, user_id: chandler.id, content: "Salut tout le monde ! Partant pour une bière aussi :D")
+Message.create(chatroom_id: chatroom.id, user_id: ross.id, content: "On se retrouve au café ? Comme d'hab ? ")
+puts "all done"
